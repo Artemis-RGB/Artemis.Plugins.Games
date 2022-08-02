4fdab37444c3360c5c0e4a8765689b66b452391d
Diogo Trindade <diogotr7@gmail.com>
2022-08-02 11:46:22 +0100

    Avalonia port

--- src/Artemis.Plugins.Games.RocketLeague/Artemis.Plugins.Games.RocketLeague.GSI/ArtemisGSI.cpp
+++ src/Artemis.Plugins.Games.RocketLeague/Artemis.Plugins.Games.RocketLeague.GSI/ArtemisGSI.cpp
@@ -182,11 +182,11 @@ void ArtemisGSI::UpdateGameState(ServerWrapper wrapper)
 }
 
 ServerWrapper ArtemisGSI::GetCurrentGameWrapper() {
-	if (gameWrapper->IsSpectatingInOnlineGame()) {
-		GameState.Status = GameStatus::Spectate;
-		return gameWrapper->GetOnlineGame();
-	}
-	else  if (gameWrapper->IsInCustomTraining()) {
+	//if (gameWrapper->IsSpectatingInOnlineGame()) {
+	//	GameState.Status = GameStatus::Spectate;
+	//	return gameWrapper->GetOnlineGame();
+	//} else  
+	if (gameWrapper->IsInCustomTraining()) {
 		GameState.Status = GameStatus::Training;
 		return gameWrapper->GetGameEventAsServer();
 	}
---
+++ src/Artemis.Plugins.Games.TruckSimulator/Views/TruckSimulatorConfigurationView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Artemis.Plugins.Modules.TruckSimulator.ViewModels;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+
+namespace Artemis.Plugins.Modules.TruckSimulator.Views
+{
+    public partial class TruckSimulatorConfigurationView : ReactiveUserControl<TruckSimulatorConfigurationViewModel>
+    {
+        public TruckSimulatorConfigurationView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
